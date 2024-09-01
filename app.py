54ac86ff36f6c7391c6cc604cfa9146d1e277990
theemadnes <alex.mattson@gmail.com>
2024-09-01 08:16:25 +0000

    move to ChatVertex

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 import gradio as gr
-from langchain_google_vertexai import VertexAI
+#from langchain_google_vertexai import VertexAI
+from langchain_google_vertexai import ChatVertexAI
 import os
 from dotenv import load_dotenv
+from langchain_core.chat_history import (
+    BaseChatMessageHistory,
+    InMemoryChatMessageHistory,
+)
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.messages import HumanMessage
+
 
 # load environment variables from .env file
 load_dotenv()
@@ -18,12 +26,14 @@ else:
     
 print(f"Setting Gradio server port to {os.getenv('GRADIO_SERVER_PORT')}")
 
-llm = VertexAI(model_name="gemini-1.5-flash", project=PROJECT_ID)
+#llm = VertexAI(model_name="gemini-1.5-flash", project=PROJECT_ID)
+model = ChatVertexAI(model="gemini-1.5-flash", project=PROJECT_ID)
 
 def generate_response(message, history):
   #ans = rag_chain({"question": message, "chat_history": chat_history})["answer"]
-  ans = llm(message)
-  return ans
+  #ans = llm(message)
+  ans = model.invoke([HumanMessage(content=message)])
+  return ans.content
 
 interface = gr.ChatInterface(fn=generate_response, examples=["Tell me about Chicago", "What was flying in the Concorde like?", "Where is the Bermuda Triangle?"], title="Chat Bot")
 interface.launch(server_name="0.0.0.0")
