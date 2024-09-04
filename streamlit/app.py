8f63015eeb36712a9025e82ffe3710015627624b
theemadnes <alex.mattson@gmail.com>
2024-09-04 07:07:40 +0000

    streamlit basic

---
+++ streamlit/app.py
@@ -0,0 +1,67 @@
+import streamlit as st
+from langchain_google_vertexai import ChatVertexAI
+import os
+from dotenv import load_dotenv
+from langchain_core.chat_history import (
+    BaseChatMessageHistory,
+    InMemoryChatMessageHistory,
+)
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.messages import HumanMessage
+import random
+import time
+
+
+# load environment variables from .env file
+load_dotenv()
+
+PROJECT_ID = os.environ.get('PROJECT_ID')   # @param {type:"string"}
+
+model = ChatVertexAI(model="gemini-1.5-flash", project=PROJECT_ID)
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = InMemoryChatMessageHistory()
+    return store[session_id]
+
+with_message_history = RunnableWithMessageHistory(model, get_session_history)
+
+config = {"configurable": {"session_id": "abc2"}} # dummy value; todo later
+
+st.title("Chat Bot")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        response = st.write_stream(response_generator())
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
+
+# Streamed response emulator
+def response_generator():
+    response = random.choice(
+        [
+            "Hello there! How can I assist you today?",
+            "Hi, human! Is there anything I can help you with?",
+            "Do you need help?",
+        ]
+    )
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
